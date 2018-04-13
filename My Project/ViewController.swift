9dbdd376adc87a1e6b49945b90c607e7ad3971aa
Unknown <bryan.arndt52@gmail.com>
2018-04-12 20:41:27 -0700

    Comment
    It was stupid!

--- My Project/ViewController.swift
+++ My Project/ViewController.swift
@@ -30,7 +30,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        
+        //Added an extra stupid comment!
         
         
     }
---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,22 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+// If Statement
+var height = 48
+if height == 48 {
+    
+    print("you can ride the ride")
+    
+}
+// Create an INT variable called temp
+// Use an if statement to see if temp is greater than 80.  If so, print IT'S HOT!
+
+var temp = 85
+
+if temp > 80 {
+    print("IT'S HOT!")
+}
